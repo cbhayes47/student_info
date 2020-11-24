947d6afb012baea88595305ec729c22036e50941
CB Hayes <cbhayes47@gmail.com>
2020-11-24 12:37:44 -0500

    Add dashboard
    Show the summary of the students on the dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    course_section_code = models.IntegerField()
+    course_department = models.CharField(max_length=30)
+    instructor = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    major = models.CharField(max_length=30)
+    year = models.CharField(max_length=30)
+    gpa = models.FloatField()
+    course1 = models.ForeignKey(
+        Course,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='student_course1',
+    )
+    course2 = models.ForeignKey(
+        Course,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='student_course2',
+    )
+    course3 = models.ForeignKey(
+        Course,
+        on_delete=models.PROTECT,
+        null=True,
+        related_name='student_course3',
+    )
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # Import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Avg
+from .models import Student
 
-class HomePageView(LoginRequiredMixin, TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = '/accounts/login/'
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        """ get_context_data let you fill the template context """
+        context = Student.objects.all().aggregate(Avg('gpa'))
+
+        context['students'] = Student.objects.all()
+        context['juniors'] = Student.objects.filter(year='Junior')
+        context['seniors'] = Student.objects.filter(year='Senior')
+        context['sophomores'] = Student.objects.filter(year='Sophomore')
+        return context
