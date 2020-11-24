664f56ab49bd86d9d30d7d9a62200504fdeb9a18
CB Hayes <cbhayes47@gmail.com>
2020-11-24 14:22:17 -0500

    Add ability to select different students for enrollment

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from django.conf.urls import url
 
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r'^$', views.DashboardView.as_view(), name='dashboard'),
     url(r'^students/$', views.StudentListView.as_view(), name='students'),
     url(r'^courses/$', views.CourseListView.as_view(), name='courses'),
+    path(r'enrollment/', views.EnrollmentView.as_view(), name='enrollment'),
+    path(r'enrollment/select_student', views.SelectStudentView),
+    re_path(r'enrollment/[0-9]+/select_student', views.SelectStudentView),
+    path(r'enrollment/<int:pk>/', views.StudentDetailView.as_view(), name='enrollment_pk'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Avg
 from .models import Student, Course
+from .forms import StudentForm
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = '/accounts/login/'
@@ -19,6 +20,18 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         return context
 
 
+class EnrollmentView(LoginRequiredMixin, TemplateView):
+    login_url = '/accounts/login/'
+    template_name = "enrollment_base.html"
+
+    def get_context_data(self, **kwargs):
+        """ get_context_data let you fill the template context """
+        context = {}
+
+        context['students'] = Student.objects.all()
+        return context
+
+
 class StudentListView(LoginRequiredMixin, ListView):
     paginate_by = 10
     model = Student
@@ -28,3 +41,27 @@ class CourseListView(LoginRequiredMixin, ListView):
     paginate_by = 10
     model = Course
     template_name = 'courses.html'
+
+class StudentDetailView(LoginRequiredMixin, DetailView): 
+    model = Student 
+    template_name = 'enrollment.html'
+    def get_context_data(self, **kwargs):
+        """ get_context_data let you fill the template context """
+        context = super().get_context_data(**kwargs)
+
+        context['students'] = Student.objects.all()
+        return context
+
+ 
+def SelectStudentView(request):
+ 
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+ 
+        if form.is_valid():
+            pk = form.cleaned_data['pk']
+            return redirect('enrollment_pk', pk=pk)
+        else:
+            return redirect('students')
+    else:
+        return redirect('enrollment')
